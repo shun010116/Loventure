0595075dc194dc31d7f576b9a6d2eca9c9a50a95
highblueberry <pianezx@naver.com>
2025-03-29 21:44:43 +0900

    register, login 페이지 hydration 오류 해결

--- loventure/src/app/layout.tsx
+++ loventure/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
+import Link from 'next/link';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,17 +19,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <header>
+      {/* Header */}
+      <header className="flex justify-between items-center border-b pb-2 mb-4">
+      <h1 className="text-2xl font-bold">Loventure</h1>
+      <div className="relative">
+        <Link href="/login"> Login </Link>
+        <button className="text-xl">🔔</button>
+        <span className="absolute -top-1 -right-1 text-xs text-red-500 font-bold">0</span>
+      </div>
+    </header>
   );
 }
--- loventure/src/app/login/page.tsx
+++ loventure/src/app/login/page.tsx
@@ -7,21 +7,27 @@ export default function Home() {
         <form method="POST" className="register">
             
             <table className="table-register">
-            <tr>
-                <th><h2>로그인</h2></th>
-            </tr>
-            <tr><td>
-                <div>&nbsp;&nbsp;이메일</div>
-                <input type="text" className="text-register" name="username"></input>
-            </td></tr>
-            <tr><td>
-                <div>&nbsp;&nbsp;비밀번호</div>
-                <input type="password" className="text-register" name="password"></input>
-            </td></tr>
+            <tbody>
+                <tr>
+                    <th><h2>로그인</h2></th>
+                </tr>
+                <tr><td>
+                    <div>&nbsp;&nbsp;이메일</div>
+                    <input type="text" className="text-register" name="username"></input>
+                </td></tr>
 
-            <tr><th><input type="submit" value="로그인" className="btn-register"></input></th></tr>
+                <tr><td>
+                    <div>&nbsp;&nbsp;비밀번호</div>
+                    <input type="password" className="text-register" name="password"></input>
+                </td></tr>
+
+                <tr><th>
+                    <input type="submit" value="로그인" className="btn-register"></input>
+                </th></tr>
+            </tbody>
             </table>
             <Link href="/register">register</Link>
+            
         </form>    
     </div>
   );
