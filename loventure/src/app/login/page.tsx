c34455cc6704fd808d67afe0383b94468d33df15
highblueberry <pianezx@naver.com>
2025-03-29 21:34:01 +0900

    로그인 및 회원가입 페이지 프로토타입

---
+++ loventure/src/app/login/page.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+
+        <form method="POST" className="register">
+            
+            <table className="table-register">
+            <tr>
+                <th><h2>로그인</h2></th>
+            </tr>
+            <tr><td>
+                <div>&nbsp;&nbsp;이메일</div>
+                <input type="text" className="text-register" name="username"></input>
+            </td></tr>
+            <tr><td>
+                <div>&nbsp;&nbsp;비밀번호</div>
+                <input type="password" className="text-register" name="password"></input>
+            </td></tr>
+
+            <tr><th><input type="submit" value="로그인" className="btn-register"></input></th></tr>
+            </table>
+            <Link href="/register">register</Link>
+        </form>    
+    </div>
+  );
+}
---
+++ loventure/src/app/register/page.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+
+export default function Home() {
+    return (
+      <div>
+  
+          <form method="POST" className="register">
+              
+              <table className="table-register">
+              <tr>
+                  <th><h2>회원가입</h2></th>
+              </tr>
+              <tr><td>
+                  <div>&nbsp;&nbsp;이름</div>
+                  <input type="text" className="text-register" name="username"></input>
+              </td></tr>
+              <tr><td>
+                  <div>&nbsp;&nbsp;이메일</div>
+                  <input type="text" className="text-register" name="email"></input>
+              </td></tr>
+              <tr><td>
+                  <div>&nbsp;&nbsp;비밀번호</div>
+                  <input type="password" className="text-register" name="password"></input>
+              </td></tr>
+              <tr><td>
+                  <div>&nbsp;&nbsp;비밀번호 확인</div>
+                  <input type="password" className="text-register" name="password2"></input>
+              </td></tr>
+  
+              <tr><th><input type="submit" value="가입하기" className="btn-register"></input></th></tr>
+              </table>
+          </form>    
+      </div>
+    );
+  }
+  
