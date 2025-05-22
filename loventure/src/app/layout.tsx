7a4d3012303397ea92683cde18e1903be457ee90
최상훈 <shchoi@g.seoultech.ac.kr>
2025-05-23 00:36:14 +0900

    오류수정

--- loventure/src/app/layout.tsx
+++ loventure/src/app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ClientLayout from "./ClientLayout";
+import "../styles/globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <body>{children}</body>
+        {children}
       </body>
     </html>
   );
